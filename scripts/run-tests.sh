744f3e3491b9be618ad55fff6f80b834c660da6d
Tim Cools <tim@soloco.be>
2015-11-18 15:59:57 +0100

    Update run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,1 +1,1 @@
- mono ./../src/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe ./../src/src\Soloco.ReactiveStarterKit.Membership.Tests\bin\Release\Soloco.ReactiveStarterKit.Membership.Tests.dll
+ mono ../src/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe ./../src/src\Soloco.ReactiveStarterKit.Membership.Tests\bin\Release\Soloco.ReactiveStarterKit.Membership.Tests.dll
