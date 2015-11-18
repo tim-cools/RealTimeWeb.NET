b6a6e7eca8f6b9059119de3e0fdf1bb0398e39a0
Tim Cools <tim@soloco.be>
2015-11-18 15:45:12 +0100

    Add run-tests.sh

---
+++ scripts/run-tests.sh
@@ -0,0 +1,1 @@
+ mono ./../src/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe ./../src/src\Soloco.ReactiveStarterKit.Membership.Tests\bin\Release\Soloco.ReactiveStarterKit.Membership.Tests.dll
