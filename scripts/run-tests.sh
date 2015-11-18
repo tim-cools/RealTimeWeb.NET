b6f14c800dad968e4dcff71be36b9e56144d23b1
Tim Cools <tim@soloco.be>
2015-11-18 17:06:38 +0100

    Fix integration tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,1 +1,1 @@
- mono ./src/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe ./src/Soloco.ReactiveStarterKit.Membership.Tests/bin/Release/Soloco.ReactiveStarterKit.Membership.Tests.dll
+ mono --runtime=4.5 ./src/packages/NUnit.Runners.2.6.4/tools/nunit-console.exe ./src/Soloco.ReactiveStarterKit.Membership.Tests/bin/Release/Soloco.ReactiveStarterKit.Membership.Tests.dll
