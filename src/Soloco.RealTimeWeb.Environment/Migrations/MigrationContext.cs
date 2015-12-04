528d9da0943fd4e3d690935ca74da0452a917efa
Tim Cools <tim@soloco.be>
2015-12-04 12:17:25 +0100

    Update environment migrator

---
+++ src/Soloco.RealTimeWeb.Environment/Migrations/MigrationContext.cs
@@ -0,0 +1,16 @@
+﻿using Soloco.RealTimeWeb.Environment.Core;
+
+namespace Soloco.RealTimeWeb.Environment.Migrations
+{
+    public class MigrationContext
+    {
+        public ILogger Logger { get;  }
+        public Settings Settings { get; }
+
+        public MigrationContext(ILogger logger, Settings settings)
+        {
+            Logger = logger;
+            Settings = settings;
+        }
+    }
+}
