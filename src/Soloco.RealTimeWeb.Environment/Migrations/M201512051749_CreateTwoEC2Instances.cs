367bdca2c22fbb98ac0f4794931ab5c7244a1f09
Tim Cools <tim@soloco.be>
2015-12-05 17:51:05 +0100

    Add EC2 empty migration

---
+++ src/Soloco.RealTimeWeb.Environment/Migrations/M201512051749_CreateTwoEC2Instances.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Soloco.RealTimeWeb.Environment.Core;
+
+namespace Soloco.RealTimeWeb.Environment.Migrations
+{
+    public class M201512051749_CreateTwoEC2Instances : IMigration
+    {
+        private readonly MigrationContext _context;
+
+        public M201512051749_CreateTwoEC2Instances(MigrationContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            _context = context;
+        }
+
+        public void Up()
+        {
+      
+        }
+
+        public void Down()
+        {
+          
+        }
+    }
+}
