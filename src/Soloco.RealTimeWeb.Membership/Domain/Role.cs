2d86c37515b46ac91f58cfbacc7b04d736f5eda6
Tim Cools <tim@soloco.be>
2016-01-08 09:55:23 +0100

    Add docs (empty)

---
+++ src/Soloco.RealTimeWeb.Membership/Domain/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Soloco.RealTimeWeb.Membership.Domain
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/Soloco.RealTimeWeb/Infrastructure/HttpContextExtensions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Authentication;
+
+namespace Soloco.RealTimeWeb.Infrastructure
+{
+    public static class HttpContextExtensions
+    {
+        public static IEnumerable<AuthenticationDescription> GetExternalProviders(this HttpContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return from description in context.Authentication.GetAuthenticationSchemes()
+                where !string.IsNullOrEmpty(description.DisplayName)
+                select description;
+        }
+
+        public static bool IsProviderSupported(this HttpContext context, string provider)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return (from description in context.GetExternalProviders()
+                where string.Equals(description.AuthenticationScheme, provider, StringComparison.OrdinalIgnoreCase)
+                select description).Any();
+        }
+    }
+}
